a68c5ea3b8d73e98b737dbc4e10ee208c58e53d4
electro0704 <118429904+electro0704@users.noreply.github.com>
2023-09-10 18:56:22 +0530

    flat_task1part2 completed

--- miscelleneous/flat_task1part2.py
+++ miscelleneous/flat_task1part2.py
@@ -6,15 +6,56 @@ def validateString(str):
             return False
     return True
 
-userIdentifiers = input("Enter the set of identifiers: ")
-identifiers = userIdentifiers.split(", ")
-# print(identifiers)
+userIdentifiers = input("Enter the space separated set of identifiers: ")
+
+identifiers = userIdentifiers.split(" ")
+print(identifiers)
+valid = True
 
 for string in identifiers:
     isValid = validateString(string)
     if(not isValid):
+        identifiers.remove(string)
+        print(identifiers)
         print("Error: invalid identifier [",string,"];")
+        valid = False
+        
+if(valid):
+    print("All identifiers are valid")
 
 userExpression = input("Enter the expression: ")
-expression = userExpression.split(" ")
-print(expression)
+ls = userExpression.split(" ")
+userExpression = "".join(ls)
+i=0 
+j=0
+valid = True
+operators = ['*', '/', '+', '-', '%']
+expression = []
+
+for symbol in userExpression:
+    
+    if(symbol in operators): # arithmetic symbol encountered
+        length = len(expression)
+        top = expression[length-1]
+        
+        if(top in operators): # current symbol as well the symbol on stack top are operators, hence invalid
+            print("Error: invalid expression (adjacent occurence of operators is not allowed);")
+            valid = False
+            break
+        
+        identifier = userExpression[i:j]
+        if(identifier not in identifiers):
+            print("Error: undeclared identifier (", identifier, "is not declared);")
+            valid = False
+            break
+        
+        expression.append(symbol)
+        j+=1
+        i=j
+            
+    else:
+        expression.append(symbol)
+        j+=1
+        
+if valid:
+    print("Expression is valid")
--- miscelleneous/flat_task1pt2.cpp
+++ miscelleneous/flat_task1pt2.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
+#include <stack>
+#include <unordered_map>
 using namespace std;
 
 bool validateString(string &str){
@@ -13,9 +16,21 @@ bool validateString(string &str){
     return true;    
 }
 
+string validateIdentifiers(vector<string> &strings){
+    // validate all the identifiers
+    for(auto str: strings){
+        bool isValid = validateString(str);
+        if(!isValid)        
+            return str;              
+    }
+
+    return "1";
+}
+
 int main(){
     vector<string> strings;
     string str;
+    unordered_map<string, bool> indentifiers;
 
     cout<<"Enter the space separated set of identifiers (enter '/stop' to terminate input stream): ";
     
@@ -27,15 +42,43 @@ int main(){
 
     }
 
-    // validate all the identifiers
-    for(auto str: strings){
-        bool isValid = validateString(str);
-        if(!isValid){
-            cout << "Error: invalid identifier [" << str << "];" << endl;
-            break;
-        }        
-    }
+    string result = validateIdentifiers(strings);
+    if(result != "1")
+        cout << "Error: invalid identifier [" << result << "];" << endl;
+    else    
+        cout << "All indentifiers are valid" << endl;
     
+    
+    //  take the expression as input from user.
+    cout << "Enter the expression: ";
+    cin >> str;
+    
+    vector<char> operators = {'*', '/', '+', '-', '%'};
+    stack<char> st;
+
+
+    int i = 0, j = 0;
+    // while (j < str.size()){
+    //     if(str[j] != )
+    // }
+    
+    for(auto symbol: str){
+        if (count(operators.begin(), operators.end(), symbol)){ // arithmetic operator encountered
+            char top = st.top();
+
+            if(count(operators.begin(), operators.end(), top))
+                cout << "Error: invalid expression (adjacent occurence of operators is not allowed);" << endl;
+            
+            
+            
+        }
+
+        else{
+
+        }
+        
+    }
+
 
 
 
